b0555ebf2c4d5b412c5fef84fbc58ef191b64fb9
Miguel <Miguel>
2022-06-13 19:27:09 -0300

    oh sii

--- funciones.js
+++ funciones.js
@@ -3,3 +3,7 @@ const recibido = a => console.log(`Se recibio ${a}`); // 1 parametro se eliminan
 
 console.log(sumar(4,5));
 recibido('Melina');
+
+const array = [1,2,3,4,5];
+
+array.forEach((elemento,index) => console.log(`${elemento} está en la posición ${index}`));
