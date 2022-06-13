ae01a599742ac5e94085486ea3d5f28937c8c8da
Miguel <Miguel>
2022-06-13 19:24:17 -0300

    comite

---
+++ funciones.js
@@ -0,0 +1,5 @@
+const sumar= (a,b)=> a+b; //2 parametros
+const recibido = a => console.log(`Se recibio ${a}`); // 1 parametro se eliminan parentesis
+
+console.log(sumar(4,5));
+recibido('Melina');
